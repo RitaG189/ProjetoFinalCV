63f156d79dbaff27de2a30b5a2ce1f3a011343a1
Rita Guimarães <kikasgui@gmail.com>
2025-01-06 19:58:12 +0000

    Fog fix

--- Assets/Scripts/ChangeTimeLight.cs
+++ Assets/Scripts/ChangeTimeLight.cs
@@ -9,28 +9,36 @@ public class ChangeTimeLight : MonoBehaviour
     [SerializeField] private GameObject sunsetLight;
     [SerializeField] private GameObject nightLight;
 
+    private bool isOvercast;
+
     private void Update() 
     { 
 
-        if(PlayerPrefs.GetInt("Time") == 1)
+        if(PlayerPrefs.GetInt("Time") == 1 && !isOvercast)
             morningLight.SetActive(true);
         else 
             morningLight.SetActive(false);
 
-        if(PlayerPrefs.GetInt("Time") == 2)
+        if(PlayerPrefs.GetInt("Time") == 2 && !isOvercast)
             dayLight.SetActive(true);
         else 
             dayLight.SetActive(false);
 
-        if(PlayerPrefs.GetInt("Time") == 3)
+        if(PlayerPrefs.GetInt("Time") == 3 && !isOvercast)
             sunsetLight.SetActive(true);
         else 
             sunsetLight.SetActive(false);
 
-        if(PlayerPrefs.GetInt("Time") == 4)
+        if(PlayerPrefs.GetInt("Time") == 4 && !isOvercast)
             nightLight.SetActive(true);
         else 
             nightLight.SetActive(false);
+
+        if ((PlayerPrefs.GetInt("isRaining") == 1 || PlayerPrefs.GetInt("isFog") == 1 || PlayerPrefs.GetInt("isThunder") == 1))
+            isOvercast = true;
+        else 
+            isOvercast = false;
+        
     }
 
 }
