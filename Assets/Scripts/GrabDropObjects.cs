56b87111603409e9c27f259832e76c44c3c60c30
DogEatTurtle <49796113+DogEatTurtle@users.noreply.github.com>
2025-01-06 21:19:51 +0000

    Render Texture museu + UI + Grab and Drop objects

--- Assets/Scripts/GrabDropObjects.cs
+++ Assets/Scripts/GrabDropObjects.cs
@@ -1,27 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GrabDropObjects : MonoBehaviour
 {
-
     [SerializeField] Transform playerCamTransform;
     [SerializeField] Transform grabPointTransform;
     [SerializeField] LayerMask pickUpLayerMask;
     private Grabbable objectGrabbable;
 
+    [SerializeField] TMP_Text textRotate;
+    [SerializeField] TMP_Text textGrab;
+    [SerializeField] TMP_Text textDrop;
+    [SerializeField] TMP_Text textExplode;
+
+    private void Start()
+    {
+        textRotate.enabled = false;
+        textGrab.enabled = false;
+        textDrop.enabled = false;
+        textExplode.enabled = false;
+    }
+
     private void Update()
     {
+        //Detectar objetos agarráveis com Raycast
+        float pickUpDistance = 2f;
+        if (Physics.Raycast(playerCamTransform.position, playerCamTransform.forward, out RaycastHit raycastHit, pickUpDistance, pickUpLayerMask))
+        {
+            //Verificar se o objeto é agarrável
+            if (raycastHit.transform.TryGetComponent<Grabbable>(out Grabbable detectedGrabbable))
+            {
+                if (raycastHit.transform.CompareTag("Explode"))
+                {
+                    textExplode.enabled = true;
+                }
+                if (objectGrabbable == null)
+                {
+                    textGrab.enabled = true;
+                }
+            }
+        }
+        else
+        {
+            if (objectGrabbable == null)
+            {
+                textGrab.enabled = false;
+                textExplode.enabled = false;
+            }
+        }
+
+        //Agarrar ou largar objetos
         if (Input.GetKeyDown(KeyCode.F))
         {
             if (objectGrabbable == null)
             {
-                float pickUpDistance = 2f;
-                if (Physics.Raycast(playerCamTransform.position, playerCamTransform.forward, out RaycastHit raycastHit, pickUpDistance, pickUpLayerMask))
+                if (Physics.Raycast(playerCamTransform.position, playerCamTransform.forward, out raycastHit, pickUpDistance, pickUpLayerMask))
                 {
                     if (raycastHit.transform.TryGetComponent(out objectGrabbable))
                     {
                         objectGrabbable.Grab(grabPointTransform);
+                        textRotate.enabled = true;
+                        textDrop.enabled = true;
+                        textGrab.enabled = false;
                     }
                 }
             }
@@ -29,6 +71,9 @@ public class GrabDropObjects : MonoBehaviour
             {
                 objectGrabbable.Drop();
                 objectGrabbable = null;
+                textRotate.enabled = false;
+                textDrop.enabled = false;
+                textGrab.enabled = false;
             }
         }
     }
